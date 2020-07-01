b83d0b1c9f82bd398a06463de621d00826d27cce
rekad97 <48532960+rekad97@users.noreply.github.com>
2020-07-01 14:02:12 +0200

    models + routing

--- src/Frontend/Undersea-Angular/src/app/features/pages/auth/auth.module.ts
+++ src/Frontend/Undersea-Angular/src/app/features/pages/auth/auth.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AuthRoutingModule } from './auth_routing';
 
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    AuthRoutingModule
+
   ]
 })
 export class AuthModule { }
--- src/Frontend/Undersea-Angular/src/app/features/pages/main/main.module.ts
+++ src/Frontend/Undersea-Angular/src/app/features/pages/main/main.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MainRoutingModule } from './main_routing';
 
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    MainRoutingModule
   ]
 })
 export class MainModule { }
