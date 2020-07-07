8c6d917e068b8de049dcc034f415b2377bce47b3
Paralike <38865450+Paralike@users.noreply.github.com>
2020-07-07 09:14:57 +0200

    Configuration files update

---
+++ src/Backend/UnderseaBackend/Undersea.DAL/Configurations/AttackConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Undersea.DAL.Models;
+
+namespace Undersea.DAL.Configurations
+{
+    class AttackConfiguration : IEntityTypeConfiguration<Attack>
+    {
+        public void Configure(EntityTypeBuilder<Attack> entity)
+        {
+            entity.HasOne(a => a.Army)
+                    .WithMany(a => a.Attacks)
+                    .HasForeignKey(a => a.ArmyId)
+                    .OnDelete(DeleteBehavior.NoAction);
+
+            entity.HasOne(a => a.AttackerCity)
+                .WithMany(c => c.Attacks)
+                .HasForeignKey(a => a.AttackerCityId)
+                .OnDelete(DeleteBehavior.NoAction);
+        }
+    }
+}
---
+++ src/Backend/UnderseaBackend/Undersea.DAL/Configurations/UnitConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Undersea.DAL.Models;
+
+namespace Undersea.DAL.Configurations
+{
+    class UnitConfiguration : IEntityTypeConfiguration<Unit>
+    {
+        public void Configure(EntityTypeBuilder<Unit> entity)
+        {
+            entity.HasKey(e => e.UnitType);
+        }
+    }
+}
