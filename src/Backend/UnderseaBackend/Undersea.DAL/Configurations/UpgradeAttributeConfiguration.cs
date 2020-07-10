6ceacf95c1ea8b0fe8485537ca4c6c3e0f749cbc
Paralike <38865450+Paralike@users.noreply.github.com>
2020-07-10 11:25:16 +0200

    update Upgrade config

--- src/Backend/UnderseaBackend/Undersea.DAL/Configurations/UpgradeAttributeConfiguration.cs
+++ src/Backend/UnderseaBackend/Undersea.DAL/Configurations/UpgradeAttributeConfiguration.cs
@@ -1,10 +1,17 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using Undersea.DAL.Models;
 
 namespace Undersea.DAL.Configurations
 {
-    class UpgradeAttributeConfiguration
+    class UpgradeAttributeConfiguration : IEntityTypeConfiguration<UpgradeAttribute>
     {
+        public void Configure(EntityTypeBuilder<UpgradeAttribute> builder)
+        {
+            builder.HasKey(u => u.Id);
+        }
     }
 }
