a3287cfd7a94b5b8de8fdd53c74c8e4296da364a
Csongor Révfalusi <revfalusi.csongor@encosoft.hu>
2020-07-13 15:54:54 +0200

    auth responsive

--- src/Frontend/Undersea-Angular/src/app/shared/nowhitespace.ts
+++ src/Frontend/Undersea-Angular/src/app/shared/nowhitespace.ts
@@ -3,6 +3,7 @@ import { FormControl } from '@angular/forms';
 export function noWhiteSpace(control: FormControl) {
     const isWhitespace = (control.value || '').trim().length === 0;
     const spaceRegex = /[\S\s]{6,}/;
+    // tslint:disable-next-line:no-shadowed-variable
     const noWhiteSpace = spaceRegex.test(control.value);
     return isWhitespace || noWhiteSpace ? { whitespace: true } : null;
 }
