12ec9b8b7aee81997b84b0752012bbd194f93285
Paralike <38865450+Paralike@users.noreply.github.com>
2020-07-13 13:38:57 +0200

    fix

--- src/Backend/UnderseaBackend/Undersea.DAL/Configurations/UpgradeAttributeConfiguration.cs
+++ src/Backend/UnderseaBackend/Undersea.DAL/Configurations/UpgradeAttributeConfiguration.cs
@@ -12,6 +12,17 @@ namespace Undersea.DAL.Configurations
         public void Configure(EntityTypeBuilder<UpgradeAttribute> builder)
         {
             builder.HasKey(u => u.Id);
+
+            builder.HasData(
+                new UpgradeAttribute
+                {
+                    upgradeType = Enums.UpgradeType.Alkimia,
+                    CoralProduction = 10,
+                    DefensePoints = 0,
+                    AttackPoints = 0,
+                    TaxIncrease = 0,
+                }
+                );
         }
     }
 }
