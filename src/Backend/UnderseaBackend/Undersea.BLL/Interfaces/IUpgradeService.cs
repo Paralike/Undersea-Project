e4b67591843a8e4620e022beea9f506c612bee6c
Paralike <38865450+Paralike@users.noreply.github.com>
2020-07-14 10:37:20 +0200

    using fix

--- src/Backend/UnderseaBackend/Undersea.BLL/Interfaces/IUpgradeService.cs
+++ src/Backend/UnderseaBackend/Undersea.BLL/Interfaces/IUpgradeService.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Undersea.BLL.DTOs;
--- src/Backend/UnderseaBackend/Undersea.DAL/Configurations/UpgradeAttributeConfiguration.cs
+++ src/Backend/UnderseaBackend/Undersea.DAL/Configurations/UpgradeAttributeConfiguration.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
 using Undersea.DAL.Models;
 
 namespace Undersea.DAL.Configurations
