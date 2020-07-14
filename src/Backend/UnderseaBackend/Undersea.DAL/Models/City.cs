eac1c48d3287c36775dfd73b311dc767e8fe88c7
Paralike <38865450+Paralike@users.noreply.github.com>
2020-07-14 10:45:58 +0200

    city

--- src/Backend/UnderseaBackend/Undersea.DAL/Models/City.cs
+++ src/Backend/UnderseaBackend/Undersea.DAL/Models/City.cs
@@ -29,7 +29,8 @@ namespace Undersea.DAL.Models
             AvailableArmyId = AvailableArmy.Id;
             AvailableArmy.CityId = Id;
             Upgrades = new Upgrade();
-            UpgradesId = Upgrades.Id;
+            //UpgradesId = Upgrades.Id;
+            Upgrades.City = this;
             Upgrades.CityId = Id;
         
         }
