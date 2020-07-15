b218b09d5f216de2695643eecc170b37ab28868e
Paralike <38865450+Paralike@users.noreply.github.com>
2020-07-15 11:47:25 +0200

    add exception and double buying protection

---
+++ src/Backend/UnderseaBackend/Undersea.BLL/Exceptions/ExistingBuildingException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Undersea.BLL.Exceptions
+{
+    public class ExistingBuildingException : Exception
+    {
+        public override string Message => "Hiba, ez az épület vagy már meg van véve, vagy valamelyik épület már épül";
+    }
+}
