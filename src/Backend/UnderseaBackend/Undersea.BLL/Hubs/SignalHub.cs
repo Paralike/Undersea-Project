6f80feee12e3858792d535b5ec65c2db00e862fe
József Tuska <tuska.jozsef@encosoft.hu>
2020-07-16 12:29:22 +0200

    SignalR Fix

--- src/Backend/UnderseaBackend/Undersea.BLL/Hubs/SignalHub.cs
+++ src/Backend/UnderseaBackend/Undersea.BLL/Hubs/SignalHub.cs
@@ -8,21 +8,21 @@ namespace Undersea.BLL.Hubs
 {
     public class SignalHub: Hub, ISignalHub
     {
-        //private readonly AppDbContext _context;
+        protected IHubContext<SignalHub> _context;
 
-        //public SignalHub(AppDbContext context)
-        //{
-        //    _context = context;
-        //}
+        public SignalHub(IHubContext<SignalHub> context)
+        {
+            _context = context;
+        }
 
         public async Task SendMessage(string user, string message)
         {
-            if(Clients == null)
+            if(_context.Clients == null)
             {
                 throw new Exception("There are no connected clients");
             }
                 
-            await Clients.All.SendAsync("NextTurn", user, message);
+            await _context.Clients.All.SendAsync("NextTurn", user, message);
         }   
     }
 }
