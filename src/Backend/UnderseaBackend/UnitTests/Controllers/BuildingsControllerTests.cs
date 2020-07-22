7dec5bd562d30d72e176679e4e465ef54abcda3b
Paralike <38865450+Paralike@users.noreply.github.com>
2020-07-22 14:13:21 +0200

    add condition

---
+++ src/Backend/UnderseaBackend/UnitTests/Controllers/BuildingsControllerTests.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Undersea.API.Controllers;
+using Undersea.BLL.Interfaces;
+using Xunit;
+
+namespace UnitTests.Controllers
+{
+    public class BuildingsControllerTests
+    {
+        [Fact]
+        public void GetBuildingsTest()
+        {
+            //var buildings = new BuildingsController(new IBuildingService() ,new IHttpContextAccessor() );
+        }
+    }
+}
